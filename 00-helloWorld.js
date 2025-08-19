b7744919f7cb470fc8f537792f3d54adecdbed09
Jesus Rumie Vittar <23528273+jrumievittar@users.noreply.github.com>
2025-08-19 20:27:40 +0200

    Prueba con operadores - mayor y menor

--- 00-helloWorld.js
+++ 00-helloWorld.js
@@ -4,3 +4,18 @@ let a = 1;
 let b = 2;
 let c = a + b;
 console.log(c);
+
+
+// This code prints "Hello world" to the console, then calculates the sum of a and b, and prints the result.
+
+
+let d = 3;
+let e = 4;
+let f = d * e;
+console.log(f);
+
+console.log("The product of d and e is: " + f);
+console.log(a < b ? "a es menor que b" : "a es mayor que b");
+console.log(a < b );
+console.log(c > f ? "c es mayor que f" : "f es mayor que c");
+console.log(c > f || c == d ? "c es mayor que f o c es igual a d" : "c no es mayor que f y c no es igual a d");
